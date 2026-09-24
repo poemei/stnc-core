f3af3dd18c3d82939bf8a4db8bac329965363d8f
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-24 15:45:59 -0700

    Core: retain presented Chain state

--- includes/stnc_core.h
+++ includes/stnc_core.h
@@ -1,6 +1,8 @@
 #ifndef STNC_CORE_H
 #define STNC_CORE_H
 
+#include <stdint.h>
+
 typedef enum stnc_core_state {
     STNC_CORE_STATE_UNINITIALIZED = 0,
     STNC_CORE_STATE_INITIALIZED,
@@ -9,11 +11,26 @@ typedef enum stnc_core_state {
     STNC_CORE_STATE_STOPPED
 } stnc_core_state;
 
+typedef struct stnc_core_chain_state {
+    int available;
+    uint8_t network_id[32];
+    uint8_t genesis_id[32];
+    uint64_t height;
+    uint8_t tip_id[32];
+    uint8_t cumulative_work[40];
+    uint8_t current_target[32];
+    uint32_t protocol_revision;
+    uint32_t block_count;
+} stnc_core_chain_state;
+
 int stnc_core_init(void);
 int stnc_core_run(void);
 void stnc_core_request_stop(void);
 void stnc_core_shutdown(void);
 
 stnc_core_state stnc_core_get_state(void);
+
+const stnc_core_chain_state *
+stnc_core_get_chain_state(void);
 
 #endif
