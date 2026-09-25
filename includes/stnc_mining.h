00f30eb367f8694bcc6ae21a7ce4a854ff0cf7d0
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 16:51:04 -0700

    Fix PoW hash input buffer size

--- includes/stnc_mining.h
+++ includes/stnc_mining.h
@@ -3,7 +3,8 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "stnc_stnc.h"
-#define STNC_MINING_HASH_INPUT_SIZE (20u+STNC_STNC_BLOCK_HEADER_SIZE)
+#define STNC_MINING_HASH_DOMAIN_SIZE 21u
+#define STNC_MINING_HASH_INPUT_SIZE (STNC_MINING_HASH_DOMAIN_SIZE+STNC_STNC_BLOCK_HEADER_SIZE)
 typedef enum stnc_mining_result {STNC_MINING_ERROR=0,STNC_MINING_FOUND=1,STNC_MINING_EXHAUSTED=2} stnc_mining_result;
 int stnc_mining_hash(const uint8_t block[STNC_STNC_BLOCK_HEADER_SIZE],uint8_t digest[32]);
 int stnc_mining_hash_meets_target(const uint8_t digest[32],const uint8_t target[32]);
