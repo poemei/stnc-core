0dd78c4d6dfab668e9cc8929b4a9e980fc31b92a
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 00:27:51 -0700

    CLI: add identity contract and wallet address commands

--- src/stnc_command.c
+++ src/stnc_command.c
@@ -4,10 +4,19 @@
 
 #include "stnc_command.h"
 #include "stnc_core.h"
+#include "stnc_stnc.h"
 
 static void stnc_command_print_usage(void)
 {
-    printf("Usage:\n  stnc-core\n  stnc-core status\n  stnc-core help\n");
+    printf(
+        "Usage:\n"
+        "  stnc-core\n"
+        "  stnc-core status\n"
+        "  stnc-core address identity <source>\n"
+        "  stnc-core address contract <source>\n"
+        "  stnc-core address wallet <source>\n"
+        "  stnc-core help\n"
+    );
 }
 
 static int stnc_command_status(void)
@@ -29,7 +38,43 @@ static int stnc_command_status(void)
         state->block_count,
         state->protocol_revision
     );
+    return 0;
+}
 
+static int stnc_command_address(int argc, char **argv)
+{
+    uint16_t type;
+    char address[STNC_STNC_ADDRESS_MAX_SIZE + 1u];
+
+    if (argc != 4) {
+        stnc_command_print_usage();
+        return 1;
+    }
+
+    if (strcmp(argv[2], "identity") == 0) {
+        type = STNC_STNC_ADDRESS_IDENTITY;
+    } else if (strcmp(argv[2], "contract") == 0) {
+        type = STNC_STNC_ADDRESS_CONTRACT;
+    } else if (strcmp(argv[2], "wallet") == 0) {
+        type = STNC_STNC_ADDRESS_WALLET;
+    } else {
+        fprintf(stderr, "Unknown address type: %s\n", argv[2]);
+        return 1;
+    }
+
+    if (argv[3][0] == '\0' ||
+        stnc_core_derive_address(
+            type,
+            (const uint8_t *)argv[3],
+            strlen(argv[3]),
+            address,
+            sizeof(address)
+        ) != 0) {
+        fprintf(stderr, "Address derivation failed.\n");
+        return 1;
+    }
+
+    printf("%s\n", address);
     return 0;
 }
 
@@ -45,6 +90,10 @@ int stnc_command_run(int argc, char **argv)
             return 1;
         }
         return stnc_command_status();
+    }
+
+    if (strcmp(argv[1], "address") == 0) {
+        return stnc_command_address(argc, argv);
     }
 
     if (strcmp(argv[1], "help") == 0 ||
