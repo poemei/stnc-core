845bf557ab11c3b2584d10bba28b35825f9e0fa4
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 13:51:33 -0700

    Reassert deterministic outputs after mining hash failure

--- src/stnc_mining.c
+++ src/stnc_mining.c
@@ -34,6 +34,8 @@ stnc_mining_result stnc_mining_search(uint8_t block[STNC_STNC_BLOCK_HEADER_SIZE]
         nonce=first_nonce+i;
         put64(block+STNC_STNC_MINING_NONCE_OFFSET,nonce);
         if(stnc_mining_hash(block,hash)!=0){
+            *found_nonce=0u;
+            memset(digest,0,32u);
             stnc_platform_secure_clear(hash,sizeof(hash));
             stnc_platform_secure_clear(target,sizeof(target));
             return STNC_MINING_ERROR;
