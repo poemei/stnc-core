63aa300179483037369331d4d63c6728ed78b1c5
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 16:21:31 -0700

    Add time-bounded CPU mining search

--- includes/stnc_mining.h
+++ includes/stnc_mining.h
@@ -9,4 +9,7 @@ int stnc_mining_hash(const uint8_t block[STNC_STNC_BLOCK_HEADER_SIZE],uint8_t di
 int stnc_mining_hash_meets_target(const uint8_t digest[32],const uint8_t target[32]);
 stnc_mining_result stnc_mining_search(uint8_t block[STNC_STNC_BLOCK_HEADER_SIZE],
     uint64_t first_nonce,uint64_t attempts,uint64_t *found_nonce,uint8_t digest[32]);
+stnc_mining_result stnc_mining_search_timed(uint8_t block[STNC_STNC_BLOCK_HEADER_SIZE],
+    uint64_t first_nonce,unsigned int budget_ms,uint64_t *attempts,
+    uint64_t *found_nonce,uint8_t digest[32]);
 #endif
