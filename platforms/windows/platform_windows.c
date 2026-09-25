32db1ef663ec8be0f07b5f16533d64448a510d57
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-24 17:34:03 -0700

    Windows: fix WinHTTP read bound constant

--- platforms/windows/platform_windows.c
+++ platforms/windows/platform_windows.c
@@ -460,7 +460,7 @@ int stnc_platform_https_get(
                 remaining = capacity - used - 1u;
 
                 if (remaining > (size_t)MAXDWORD) {
-                    remaining = (size_t)DWORD_MAX;
+                    remaining = (size_t)MAXDWORD;
                 }
 
                 received = 0;
