7ea546d50798c9c0a1a7e03c93b4fdbd8b78b3be
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 01:22:34 -0700

    Core: expose transaction submission

--- includes/stnc_core.h
+++ includes/stnc_core.h
@@ -46,5 +46,6 @@ int stnc_core_derive_address(
 
 int stnc_core_balance(const char *wallet, uint64_t *units);
 int stnc_core_contract_state(const char *contract, stnc_contract_state *state);
+int stnc_core_submit_transaction(const uint8_t *transaction,size_t transaction_length,stnc_submission_result *result);
 
 #endif
