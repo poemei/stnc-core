2ac7f44f114891ab8a6b32a0b816d047cd3c3458
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 01:19:18 -0700

    Platform: expose private-file protection

--- includes/stnc_platform.h
+++ includes/stnc_platform.h
@@ -51,6 +51,7 @@ int stnc_platform_https_get(
 int stnc_platform_random(unsigned char *buffer,size_t length);
 int stnc_platform_sha256(const unsigned char *buffer,size_t length,unsigned char digest[32]);
 void stnc_platform_secure_clear(void *buffer,size_t length);
+int stnc_platform_protect_private_file(const char *path);
 
 uint64_t stnc_platform_monotonic_ms(void);
 
