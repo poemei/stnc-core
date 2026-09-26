006267623f51fd47b54c39813f76cdda71592706
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 20:22:09 -0700

    Make Bitcoin style GUI the default STNC desktop

--- src/main.c
+++ src/main.c
@@ -6,18 +6,12 @@
 #include "stnc_log.h"
 #include "stnc_gui.h"
 
-/* Keep the established Windows GUI translation unit available while the
- * compact client replaces it as the default desktop presentation. */
-#define stnc_gui_run stnc_gui_compact_run
-#include "../platforms/windows/gui_compact.c"
-#undef stnc_gui_run
-
 int main(int argc,char **argv)
 {
     int result;
     int command_mode;
 
-    if(argc==1||(argc==2&&strcmp(argv[1],"gui")==0))return stnc_gui_compact_run();
+    if(argc==1||(argc==2&&strcmp(argv[1],"gui")==0))return stnc_gui_bitcoin_run();
     printf("STNC Core\n");
     if(argc==2&&strcmp(argv[1],"run")==0)argc=1;
     command_mode=argc>1;
