510997bf4a6c9a949734449296264d590a52627f
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-25 18:28:47 -0700

    Build and qualify STNC transfer service

--- build.cmd
+++ build.cmd
@@ -5,14 +5,12 @@ if /I "%~1"=="clean" (
     if exist build (
         echo Removing build directory...
         rmdir /S /Q build
-
         if exist build (
             echo.
             echo CLEAN FAILED
             exit /b 1
         )
     )
-
     echo.
     echo CLEAN SUCCESSFUL
     exit /b 0
@@ -46,6 +44,7 @@ cl /nologo /W4 /TC /Iincludes ^
     src\stnc_peer_select.c ^
     src\stnc_stnc.c ^
     src\stnc_stnp.c ^
+    src\stnc_transfer.c ^
     src\stnc_wallet.c ^
     src\stnc_wallet_store.c ^
     src\stnc_wallet_status.c ^
@@ -61,204 +60,89 @@ if errorlevel 1 (
 )
 
 cl /nologo /W4 /TC /D_CRT_SECURE_NO_WARNINGS /Iincludes tests\test_stnc_log.c src\stnc_log.c /Fe:build\test-stnc-log.exe
-if errorlevel 1 (
-    echo.
-    echo LOG TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo LOG TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-log.exe
-if errorlevel 1 (
-    echo.
-    echo LOG TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo LOG TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_stnp.c src\stnc_stnp.c /Fe:build\test-stnc-stnp.exe
-if errorlevel 1 (
-    echo.
-    echo TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-stnp.exe
-if errorlevel 1 (
-    echo.
-    echo TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_peers.c src\stnc_peers.c /Fe:build\test-stnc-peers.exe
-if errorlevel 1 (
-    echo.
-    echo PEER TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo PEER TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-peers.exe
-if errorlevel 1 (
-    echo.
-    echo PEER TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo PEER TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_directory.c src\stnc_directory.c src\stnc_peers.c /Fe:build\test-stnc-directory.exe
-if errorlevel 1 (
-    echo.
-    echo DIRECTORY TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo DIRECTORY TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-directory.exe
-if errorlevel 1 (
-    echo.
-    echo DIRECTORY TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo DIRECTORY TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_http.c src\stnc_http.c /Fe:build\test-stnc-http.exe
-if errorlevel 1 (
-    echo.
-    echo HTTP TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo HTTP TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-http.exe
-if errorlevel 1 (
-    echo.
-    echo HTTP TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo HTTP TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_peer_select.c src\stnc_peer_select.c /Fe:build\test-stnc-peer-select.exe
-if errorlevel 1 (
-    echo.
-    echo PEER SELECT TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo PEER SELECT TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-peer-select.exe
-if errorlevel 1 (
-    echo.
-    echo PEER SELECT TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo PEER SELECT TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_stnc.c src\stnc_stnc.c /Fe:build\test-stnc-stnc.exe
-if errorlevel 1 (
-    echo.
-    echo STNC CODEC TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STNC CODEC TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-stnc.exe
-if errorlevel 1 (
-    echo.
-    echo STNC CODEC TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STNC CODEC TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_mining.c src\stnc_mining.c /Fe:build\test-stnc-mining.exe
-if errorlevel 1 (
-    echo.
-    echo MINING TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo MINING TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-mining.exe
-if errorlevel 1 (
-    echo.
-    echo MINING TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo MINING TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_mining_service.c src\stnc_mining_service.c /Fe:build\test-stnc-mining-service.exe
-if errorlevel 1 (
-    echo.
-    echo MINING SERVICE TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo MINING SERVICE TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-mining-service.exe
-if errorlevel 1 (
-    echo.
-    echo MINING SERVICE TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo MINING SERVICE TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_stratum.c src\stnc_stratum.c /Fe:build\test-stnc-stratum.exe
-if errorlevel 1 (
-    echo.
-    echo STRATUM PROTOCOL TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STRATUM PROTOCOL TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-stratum.exe
-if errorlevel 1 (
-    echo.
-    echo STRATUM PROTOCOL TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STRATUM PROTOCOL TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_stratum_client.c src\stnc_stratum_client.c src\stnc_stratum.c /Fe:build\test-stnc-stratum-client.exe
-if errorlevel 1 (
-    echo.
-    echo STRATUM CLIENT TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STRATUM CLIENT TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-stratum-client.exe
-if errorlevel 1 (
-    echo.
-    echo STRATUM CLIENT TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo STRATUM CLIENT TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /D_CRT_SECURE_NO_WARNINGS /Iincludes tests\test_stnc_background_mining.c src\stnc_background_mining.c src\stnc_mining_service.c src\stnc_log.c /Fe:build\test-stnc-background-mining.exe
-if errorlevel 1 (
-    echo.
-    echo BACKGROUND MINING TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo BACKGROUND MINING TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-background-mining.exe
-if errorlevel 1 (
-    echo.
-    echo BACKGROUND MINING TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo BACKGROUND MINING TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_wallet.c src\stnc_wallet.c src\stnc_core.c src\stnc_background_mining.c src\stnc_stratum_client.c src\stnc_stratum.c src\stnc_mining_service.c src\stnc_mining.c src\stnc_wallet_store.c src\stnc_log.c src\stnc_config.c src\stnc_directory.c src\stnc_http.c src\stnc_network.c src\stnc_peers.c src\stnc_peer_select.c src\stnc_stnc.c src\stnc_stnp.c platforms\windows\platform_windows.c src\crypto\ed25519_donna\ed25519_provider.c /Fe:build\test-stnc-wallet.exe /link ws2_32.lib winhttp.lib bcrypt.lib advapi32.lib
-if errorlevel 1 (
-    echo.
-    echo WALLET TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-wallet.exe
-if errorlevel 1 (
-    echo.
-    echo WALLET TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /D_CRT_SECURE_NO_WARNINGS /Iincludes tests\test_stnc_wallet_store.c src\stnc_wallet_store.c src\stnc_wallet.c platforms\windows\platform_windows.c src\crypto\ed25519_donna\ed25519_provider.c src\stnc_core.c src\stnc_background_mining.c src\stnc_stratum_client.c src\stnc_stratum.c src\stnc_mining_service.c src\stnc_mining.c src\stnc_log.c src\stnc_config.c src\stnc_directory.c src\stnc_http.c src\stnc_network.c src\stnc_peers.c src\stnc_peer_select.c src\stnc_stnc.c src\stnc_stnp.c /Fe:build\test-stnc-wallet-store.exe /link ws2_32.lib winhttp.lib bcrypt.lib advapi32.lib
-if errorlevel 1 (
-    echo.
-    echo WALLET STORE TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET STORE TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-wallet-store.exe
-if errorlevel 1 (
-    echo.
-    echo WALLET STORE TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET STORE TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_wallet_status.c src\stnc_wallet_status.c /Fe:build\test-stnc-wallet-status.exe
-if errorlevel 1 (
-    echo.
-    echo WALLET STATUS TEST BUILD FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET STATUS TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-wallet-status.exe
-if errorlevel 1 (
-    echo.
-    echo WALLET STATUS TEST FAILED
-    exit /b 1
-)
+if errorlevel 1 ( echo. & echo WALLET STATUS TEST FAILED & exit /b 1 )
 
 cl /nologo /W4 /TC /Iincludes tests\test_stnc_contract_status.c src\stnc_contract_status.c /Fe:build\test-stnc-contract-status.exe
-if errorlevel 1 exit /b 1
+if errorlevel 1 ( echo. & echo CONTRACT STATUS TEST BUILD FAILED & exit /b 1 )
 build\test-stnc-contract-status.exe
-if errorlevel 1 exit /b 1
+if errorlevel 1 ( echo. & echo CONTRACT STATUS TEST FAILED & exit /b 1 )
+
+cl /nologo /W4 /TC /Iincludes tests\test_stnc_transfer.c src\stnc_transfer.c /Fe:build\test-stnc-transfer.exe
+if errorlevel 1 ( echo. & echo TRANSFER TEST BUILD FAILED & exit /b 1 )
+build\test-stnc-transfer.exe
+if errorlevel 1 ( echo. & echo TRANSFER TEST FAILED & exit /b 1 )
 
 echo.
 echo BUILD SUCCESSFUL
